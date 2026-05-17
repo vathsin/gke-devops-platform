1e46d878dfbc1839c0470562187391f6a052caa7
Vathsin <vathsinb@gmail.com>
2026-05-17 13:44:36 +0530

    Reduced GKE node disk size

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -16,9 +16,14 @@ resource "google_container_node_pool" "primary_nodes" {
   node_count = 1
 
   node_config {
-    machine_type = "e2-medium"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
+  machine_type = "e2-small"
+
+  disk_size_gb = 20
+
+  disk_type = "pd-standard"
+
+  oauth_scopes = [
+    "https://www.googleapis.com/auth/cloud-platform"
+  ]
+}
 }
